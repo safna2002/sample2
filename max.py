cfd60adeab0363f2acbca7f1ad70be1c5a874cc4
safna2002 <safnafathima033@gmail.com>
2024-02-18 13:50:04 +0530

    Assignment 2

---
+++ max.py
@@ -0,0 +1,30 @@
+w="Grass is greener on the other side"
+s=""
+r=""
+x=""
+c=""
+if w.upper():
+    w=w.lower()
+for i in w:
+    if i not in s:
+        s=s+i
+    else:
+        r=r+i
+        r=r.replace(" ",'')
+c={}
+for j in r:
+    if j in c:
+        c[j]+=1
+    else:
+        c[j]=1
+m = 0
+mr= ""
+for j, c in c.items():
+    if c > m:
+        m = c
+        mr=j
+
+print("Most repeating letter is",mr)
+
+
+
